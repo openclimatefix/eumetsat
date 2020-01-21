6e59c191bffa9a1be794f5ff8b1afdcf29809edc
Jack Kelly <jack-list@xlk.org.uk>
2020-01-21 17:19:55 +0000

    add check_processed_files

--- eumetsat.py
+++ eumetsat.py
@@ -1,6 +1,15 @@
 import os
+import re
+from datetime import datetime
 
 
 def get_filesize_megabytes(filename):
     filesize_bytes = os.path.getsize(filename)
     return filesize_bytes / 1E6
+
+
+def eumetsat_filename_to_datetime(inner_tar_name):
+    p = re.compile('^MSG[23]-SEVI-MSG15-0100-NA-(\d*)\.')
+    title_match = p.match(inner_tar_name)
+    date_str = title_match.group(1)
+    return datetime.strptime(date_str, "%Y%m%d%H%M%S")
